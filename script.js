a187e7b844ded31988bc6d42beff462e7fe24ace
caio souza <caiocbahia@hotmail.com>
2022-02-24 22:17:03 -0300

    Corrigindo indentação do JS

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
-// let banco = [
-// 	{'tarefa': 'Estudar', 'status': 'checked'},
-// 	{'tarefa': 'Passear', 'status': ''},
-// ]
+let banco = []
 
 const getBanco = () => JSON.parse(localStorage.getItem('todoList')) ?? []
 const setBanco = (banco) => localStorage.setItem('todoList', JSON.stringify(banco))
 
-const criarItem = (tarefa, status, indice) =>{
+const criarItem = (tarefa, status, indice) => {
 	const item = document.createElement('label')
 	item.classList.add('todo__item')
 	item.innerHTML = `
-	<input type="checkbox" ${status} data-indice=${indice}>
+	<input type='checkbox' ${status} data-indice=${indice}>
 	<div>${tarefa}</div>
-	<input type="button" value="X" data-indice=${indice}>
+	<input type='button' value='X' data-indice=${indice}>
 	`
-
 	document.getElementById('todoList').appendChild(item)
 }
 
-const limparTarefas = () => {
+const limparTarefas = () =>{
 	const todoList = document.getElementById('todoList')
 	while(todoList.firstChild){
-			todoList.removeChild(todoList.lastChild)
+		todoList.removeChild(todoList.lastChild)
 	}
 }
 
 const atualizarTela = () => {
 	const banco = getBanco()
 	limparTarefas()
-	banco.forEach((item,indice) => criarItem(item.tarefa, item.status, indice))
+	banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice))
+}
+
+const adicionarItem = (evento) => {
+	const tecla = evento.key 
+	const texto = evento.target.value 
+	if(tecla === "Enter"){
+		const banco = getBanco()
+		banco.push({'tarefa': texto, 'status': ''})
+		setBanco(banco)
+		atualizarTela()
+		evento.target.value = ''
+	}
 }
 
 const removerItem = (indice) => {
@@ -45,27 +53,16 @@ const atualizarItem = (indice) => {
 	atualizarTela()
 }
 
-const clickItem = (evento) =>{
-	const elemento = evento.target 
+const clickItem = (evento) => {
+	const elemento = evento.target
 	if(elemento.type === "button"){
-			const indice = elemento.dataset.indice 
-			removerItem(indice)
-	} else if (elemento.type === 'checkbox'){
+		const indice = elemento.dataset.indice 
+		removerItem(indice)
+	}else if(elemento.type === "checkbox"){
 		const indice = elemento.dataset.indice 
 		atualizarItem(indice)
 	}
-}
 
-const adicionarItem = (evento) =>{
-	const tecla = evento.key 
-	const texto = evento.target.value 
-	if(tecla === "Enter"){
-			const banco = getBanco()
-			banco.push({'tarefa': texto, 'status': ''})
-			setBanco(banco)
-			atualizarTela()
-			evento.target.value = ''
-	}
 }
 
 document.getElementById('newItem').addEventListener('keypress', adicionarItem)
